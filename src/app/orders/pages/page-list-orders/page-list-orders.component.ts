623bb386b887b8b4c56ff5295902d1464cb1f2b7
dnyuhm <dylanmeuriot@gmail.com>
2022-03-24 11:53:12 +0100

    Commit 28 : Pipe Angular &  changeState

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -11,11 +12,13 @@ import { OrdersService } from '../../services/orders.service';
 export class PageListOrdersComponent implements OnInit {
   public myTitle: string;
   public collection$: Observable<Order[]>;
+  public states: string[];
   public headers: string[];
 
   constructor(private ordersService: OrdersService) {
     this.myTitle = 'Orders list';
     this.collection$ = this.ordersService.collection$;
+    this.states = Object.values(StateOrder);
     this.headers = [
       'Type',
       'client',
@@ -30,6 +33,13 @@ export class PageListOrdersComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  public changeState(item: Order, event: any) {
+    const state = event.target.value;
+    this.ordersService
+      .changeState(item, state)
+      .subscribe((data) => Object.assign(item, data));
+  }
 
   // public changeTitle(): void {
   //   this.myTitle = 'My List Orders (titre change)';
